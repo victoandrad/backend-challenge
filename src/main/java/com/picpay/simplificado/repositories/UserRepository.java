e05a3d8c2274abc861c51dbbeede8d1daa8ffdc0
Victor Andrade <victor64gaming@gmail.com>
2025-01-15 20:01:30 -0300

    feat: changed return of UserRepository methods

--- src/main/java/com/picpay/simplificado/repositories/UserRepository.java
+++ src/main/java/com/picpay/simplificado/repositories/UserRepository.java
@@ -4,10 +4,12 @@ import com.picpay.simplificado.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByDocument(String document);
+    Optional<User> findByDocument(String document);
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
