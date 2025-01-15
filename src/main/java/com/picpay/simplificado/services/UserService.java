57263d22bc6e4416b3b2d6626b5a4fe35206c598
Victor Andrade <victor64gaming@gmail.com>
2025-01-15 20:01:03 -0300

    feat: added crud methods in UserService

--- src/main/java/com/picpay/simplificado/services/UserService.java
+++ src/main/java/com/picpay/simplificado/services/UserService.java
@@ -1,13 +1,14 @@
 package com.picpay.simplificado.services;
 
-import com.picpay.simplificado.domain.transaction.Transaction;
 import com.picpay.simplificado.domain.user.User;
-import com.picpay.simplificado.domain.user.UserType;
+import com.picpay.simplificado.dtos.UserDTO;
 import com.picpay.simplificado.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,14 +21,9 @@ public class UserService {
         this.repository = repository;
     }
 
-    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
-        if (sender.getUserType().equals(UserType.MERCHANT)) {
-            throw new Exception("Sender can not to do transactions");
-        }
-
-        if (sender.getBalance().compareTo(amount) < 0) {
-            throw new Exception("Sender do not have enough money");
-        }
+    public User createUserFromDTO(UserDTO dto) throws Exception {
+        User user = new User(dto);
+        return this.insert(user);
     }
 
     public User findById(Long id) throws Exception {
@@ -35,7 +31,42 @@ public class UserService {
         return user.orElseThrow(() -> new Exception("Resource Not Found"));
     }
 
+    public User findByDocument(String document) throws Exception {
+        Optional<User> user = this.repository.findByDocument(document);
+        return user.orElseThrow(() -> new Exception("Resource Not Found"));
+    }
+
+    public User findByEmail(String email) throws Exception {
+        Optional<User> user = this.repository.findByEmail(email);
+        return user.orElseThrow(() -> new Exception("Resource Not Found"));
+    }
+
+    public List<User> findAll() throws Exception {
+        return this.repository.findAll();
+    }
+
     public User insert(User user) {
         return this.repository.save(user);
+    }
+
+    public void delete(Long id) throws Exception {
+        try {
+            this.repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new Exception(e.getMessage());
+        } catch (EmptyResultDataAccessException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public User update(Long id, UserDTO dto) throws Exception {
+        User user = this.findById(id);
+        user.setFirstName(dto.firstName());
+        user.setLastName(dto.lastName());
+        user.setDocument(dto.document());
+        user.setEmail(dto.email());
+        user.setPassword(dto.password());
+        user.setUserType(dto.userType());
+        return this.insert(user);
     }
 }
